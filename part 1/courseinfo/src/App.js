68eb97baee214f1a4611b207a9da6bcdb06c760a
Diego Andres Dimuro <dimurodiego@gmail.com>
2022-08-13 21:23:45 -0300

    1.1 app.js modifications

--- part 1/courseinfo/src/App.js
+++ part 1/courseinfo/src/App.js
@@ -1,3 +1,44 @@
+
+// bloque para mostrar el nombre del curso
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+// bloque para mostrar cantidad de ejercicios
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+// bloque para mostrar numero de ejercicios
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+// usar componentes separados para mostrar la info
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -8,20 +49,12 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </>
   )
 }
 
-export default App
+export default App 
